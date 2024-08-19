3cea6723d0bea65ce070691332f28b22caeee813
Lê Hoàng <103761864+hoangle1269@users.noreply.github.com>
2024-08-20 05:55:10 +0700

    1st

---
+++ src/main/java/com/example/customerprovincemanagement/service/IGenerateService.java
@@ -0,0 +1,15 @@
+package com.example.customerprovincemanagement.service;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+public interface IGenerateService<T> {
+    Iterable<T> findAll();
+
+    void save(T t);
+
+    Optional<T> findById(Long id);
+
+    void remove(Long id);
+}
